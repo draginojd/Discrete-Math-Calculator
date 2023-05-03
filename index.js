17dd67c732fc52017154a32e316e159b001c0049
draginojd <arminfazlikhan@gmail.com>
2023-05-03 18:50:25 +0200

    each set a and b enclosed in curly brackets

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-/* function calculateCartesianProduct() {
-    var setA = document.getElementById("setA").value.split(",");
-    var setB = document.getElementById("setB").value.split(",");
-    var cartesianProduct = [];
-  
-    for (var i = 0; i < setA.length; i++) {
-      for (var j = 0; j < setB.length; j++) {
-        cartesianProduct.push("(" + setA[i] + ", " + setB[j] + ")");
-      }
-    }
-  
-    cartesianProduct = removeInnerBrackets(cartesianProduct);
-  
-    document.getElementById("result").innerHTML = "Cartesian Product: {" + cartesianProduct.join(", ") + "}";
-  } */
+
   function calculateCartesianProduct() {
     var input = document.getElementById("set").value;
     
-    if (!input.includes(",")) {
-      alert("Please enter two sets separated by a comma (e.g. '1,5,1,2')");
+    if (!input.includes(", ")) {
+      alert("Please enter two sets separated by a comma and a space (e.g. '{1, 5}, {1, 2}')");
       return;
     }
     
-    var sets = input.split(" , ");
-    var setA = sets[0].trim().split(",");
-    var setB = sets[1].trim().split(",");
+    var sets = input.split(", ");
+    var setA = sets[0].trim().replace("{", "").replace("}", "").split(",");
+    var setB = sets[1].trim().replace("{", "").replace("}", "").split(",");
     var cartesianProduct = [];
   
     for (var i = 0; i < setA.length; i++) {
@@ -34,8 +20,6 @@
   
     document.getElementById("result").innerHTML = "Cartesian Product: {" + cartesianProduct.join(", ") + "}";
   }
-  
-
   
   
   function removeInnerBrackets(arr) {
@@ -52,14 +36,14 @@
   }
   
   
-function appendToDisplay(value) {
+  function appendToDisplay(value) {
     document.getElementById("set").value += value;
   }
   
   function clearDisplay() {
     document.getElementById("set").value = "";
   }
-
+  
   function performAddition() {
     var input = document.getElementById("set").value;
   
